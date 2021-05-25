98baf7fb42a16352ffb6cd93e298d7494066c5e0
asselot <80883289+asselot@users.noreply.github.com>
2021-05-25 21:48:00 +0200

    Update TextViewer.h

--- TextViewer.h
+++ TextViewer.h
@@ -2,8 +2,11 @@ pragma once
 # include <iostream>
 # include "SupportADessin"
 
+// Classe qui va permettre d'afficher en mode texte Systeme, Montagne et Ciel. C'est donc un support à dessin
+// Va afficher les différentes composantes de Systeme, Montagne et Ciel en fonction des méthodes affiche() de chacun
 
 class TextViewer : public SupportADessin
+
 {
 virtual void dessine(Systeme const&);
 virtual void dessine(Montagne const&);

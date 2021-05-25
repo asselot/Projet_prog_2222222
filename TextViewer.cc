5377e502439e36d804fc9c1dff6c183f7e2c1187
asselot <80883289+asselot@users.noreply.github.com>
2021-05-25 21:48:25 +0200

    Update TextViewer.cc

--- TextViewer.cc
+++ TextViewer.cc
@@ -1,20 +1,22 @@
 # include <iostream>
 # include "SupportADessin"
 
+// afficher les différents composants de Systeme, Montagne et Ciel en fonction des méthodes affiche() de chacun
 
 void TextViewer :: dessine(Systeme const& a_dessiner)
 {
-  
-  // afficher les différents + faire un constructeur de textviewer + support à dessin
+	a_dessiner. affiche(); 
 }
+
 void TextViewer :: dessine(Montagne const& a_dessiner)
 {
-  
-  
+	a_dessiner. affiche_para(); 
 }
+
 void TextViewer :: dessine(Ciel const& a_dessiner)
 {
+	a_dessiner.affiche();
 }
-	
+		
 	
 

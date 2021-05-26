4b0e4d03f17af06e2f7a5ee94235e6bf3d94ee27
asselot <80883289+asselot@users.noreply.github.com>
2021-05-26 21:22:01 +0200

    Update Boite3D.h

--- Boite3D.h
+++ Boite3D.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <iostream>
+#include <string>
 
+template <typename T1>
 
 class Boite3D
 {
@@ -16,4 +18,10 @@ class Boite3D
 	unsigned int Ny;
 	unsigned int Nz;
 	double pas;
+	
+	private : 
+	
+	
+// on cherche a pouvoir creer un triple vector dont on peut choisir le type dans les classes suivantes et dont on pourrais changer le nom	
+ std::vector<std::vector<std::vector<T1>>> nom; 
 };

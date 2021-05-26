b23cbcb9ebd20802865b2b6e34a454014bcb0252
asselot <80883289+asselot@users.noreply.github.com>
2021-05-26 12:26:34 +0200

    Update TextViewer.h

--- TextViewer.h
+++ TextViewer.h
@@ -1,6 +1,9 @@
 pragma once 
 # include <iostream>
 # include "SupportADessin.h"
+# include "Montagne.h"
+# include "Systeme.h"
+# include "Ciel.h"
 
 // La classe Terminal indique que l'on souhaite afficher la simulation sous forme textuelle sur le terminal
 // Va afficher les différentes composantes de Systeme, Montagne et Ciel en fonction des méthodes affiche() de chacun

7223d3349fbd4c877340cb124e34d295f9a4ec51
asselot <80883289+asselot@users.noreply.github.com>
2021-05-26 12:08:54 +0200

    Update SupportADessin.h

--- SupportADessin.h
+++ SupportADessin.h
@@ -1,17 +1,29 @@
 #pragma once
 #include <iostream>
 
-// Super-classe qui est dédiée à la « visualisation » de la modélisation
+// Super-classe qui est dédiée à la « visualisation » de la modélisation (mode texte ou graphique)
+// Classe abstraite dont hérite TextViewer (texte) et Open_vueGL (graphisme)
 
 class SupportADessin 
 {
 	public:
 	
 virtual ~SupportADessin() = default;
-// on suppose ici que les supports ne seront ni copiés ni déplacés
+// On suppose ici que les supports ne seront ni copiés ni déplacés
+
+// Méthodes virtuelles pures qui indiquent comment afficher les différents objets de la simulation
+// Ces méthodes sont pures car la classe ne sait pas encore comment afficher les elements dessinables
 
 virtual void dessine(Systeme const&) = 0;
 virtual void dessine(Montagne const&) = 0;
 virtual void dessine(Ciel const&) = 0;
-
+	
+protected : 
+	
+// Permet d'éviter les copies de support à dessin, qui n'auraient pas de sens 
+	
+SupportADessin(SupportADessin const&) = delete;
+SupportADessin& operator=(SupportADessin const&) = delete;
+			
+	
 };

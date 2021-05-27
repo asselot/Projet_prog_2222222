4b7c709cfdbe77246554c1f16095356645da52c1
asselot <80883289+asselot@users.noreply.github.com>
2021-05-27 11:57:47 +0200

    Update Boite3D.h

--- Boite3D.h
+++ Boite3D.h
@@ -2,20 +2,33 @@
 #include <iostream>
 #include <string>
 
-template <typename T1>
+// Super classe qui définit une boîte en 3D où va être définit le système physique
+// Ciel et ChampsPotentiels sont des boîte 3D --> héritage
+
+// T : variable utilisée dans le triple tableau, CubedAir pour le Ciel et des Potentiels pour le champ de potentiels
+template <typename T>
 
 class Boite3D
 {
 	public:
+	
+	// Constructeur
 	Boite3D(unsigned int nomx, unsigned int nomy, unsigned int nomz, double ps) : Nx(nomx), Ny(nomy), Nz(nomz), pas(ps) {}
-	int getNx();
-	int getNy();
-	int getNz();
+	
+	// Getteurs
+	int getNx() const;
+	int getNy() const;
+	int getNz() const;
 	double getpas();
 	
-	virtual void affiche() = 0;
+	// Getteur en virtuel pur car le triple tableau sera de nature différente pour les sous classes
+	std::vector<std::vector<std::vector<T>>> get_tablO() const = 0;
+	
+	// Méthode affiche définie différemment pour les sous classes, d'où la méthode virtuelle pure
+	virtual void affiche() const = 0;
 	
 	protected:
+	
 	unsigned int Nx;
 	unsigned int Ny;
 	unsigned int Nz;
@@ -23,7 +36,7 @@ class Boite3D
 	
 	private : 
 	
+        // On cherche a pouvoir créer un triple vector dont on peut choisir le type dans les sous classes	
+        std::vector<std::vector<std::vector<T>>> tablO; 
 	
-// on cherche a pouvoir creer un triple vector dont on peut choisir le type dans les classes suivantes et dont on pourrais changer le nom	
- std::vector<std::vector<std::vector<T1>>> nom; 
 };

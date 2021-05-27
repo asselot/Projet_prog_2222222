1c350220e1fa4b90f6cf089263e1662cabd11650
asselot <80883289+asselot@users.noreply.github.com>
2021-05-27 13:25:04 +0200

    Create Ciel.h

--- Ciel.h
+++ Ciel.h
@@ -1,19 +1,21 @@
 #prama once 
 #include <iostream>
+#include <vector>
 #include "Boite3D.h"
 #include "CubeAir.h"
 #include "Dessinable.h"
 #include "SupportADessin.h"
 
-class Ciel : public Boite3D<CubedAir> Dessinable
+class Ciel : public Boite3D <CubeAir> Dessinable
 
 {
 	friend class CubedAir;
 	public:
 	virtual void dessine_sur(SupportADessin a_dessiner)
-	
 	Ciel(const ChampsPotentiels& champ) : Boite3D(champ.Nx, champ.Ny, champ.Nz, champ.pas) {}
 	
 	
 	
+	
+	// constructeurs+ dessinesur+ affichage
 };

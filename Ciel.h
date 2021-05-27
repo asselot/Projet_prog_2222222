5faaa38645f9259708711ff4a3cb3876d013e768
asselot <80883289+asselot@users.noreply.github.com>
2021-05-27 11:09:41 +0200

    Update Ciel.h

--- Ciel.h
+++ Ciel.h
@@ -1,22 +1,19 @@
 #prama once 
 #include <iostream>
-#include <vector>
 #include "Boite3D.h"
 #include "CubeAir.h"
 #include "Dessinable.h"
 #include "SupportADessin.h"
 
-class Ciel : public Boite3D Dessinable
+class Ciel : public Boite3D<CubedAir> Dessinable
 
 {
 	friend class CubedAir;
 	public:
 	virtual void dessine_sur(SupportADessin a_dessiner)
+	
 	Ciel(const ChampsPotentiels& champ) : Boite3D(champ.Nx, champ.Ny, champ.Nz, champ.pas) {}
 	
 	
-	private:
-	std::vector<std::vector<std::vector<CubedAir>>> cubes_dair;
 	
-	// constructeurs+ dessinesur+ affichage
 };

7a71cbf35669487abea7a81f7006e6bf8f98e8d2
asselot <80883289+asselot@users.noreply.github.com>
2021-05-27 18:20:35 +0200

    Update ChampsPotentiels.h

--- ChampsPotentiels.h
+++ ChampsPotentiels.h
@@ -1,33 +1,54 @@
-#pragma once 
+#pragma once
 #include <iostream>
-#include <array>
 #include <vector>
-#include "Boite3D"
-#include "Vecteur2D"
-#include "Potentiel"
+#include <array>
+#include "Vecteur2D.h"
+#include "constantes.h"
+#include "Boite3D.h"
+#include "Potentiel.h"
+#include "Montagne.h"
 
 
-class ChampsPotentiels : public Boite3D
+class ChampsPotentiels : public Boite3D<Potentiel>
 {
-	friend class Ciel;
+	
 	public:
+
+	
+	// Contructeur qui va construir par défaut une boîte 30x30x30 cubes de pas 1
 	ChampsPotentiels(unsigned int nox = 30, unsigned int noy = 30, unsigned int noz = 30, double p = 1) : Boite3D(nox, noy, noz, p) {}
+	
+	
+//--------------------------------------------------------------METHODES--------------------------------------------------------------------//
 
-	void initialise(double vi, const Montagne& mont);
+        
+	// Initialise les potentiels de la boîte 
+	void initialise (double vi, const Montagne& mont);
 	
-	bool estauxbords(Potentiel pot) const;
 	
+	// Retourne true si un potentiel (vecteur) est au bord de la boîte
+	bool estauxbords (Potentiel pot) const;
+	
+	
+	// Calcule le laplacien en tout point intérieur de la boîte
 	void calcule_laplaciens();
-	double erreur();
-	void iteration( double E = 0.1);
-	void resolution ( double seuil = 2.2621843e-5, unsigned int max = 5000, bool verbeuse = false );
-	std::array<double, 3> vitesse( unsigned int i, unsigned int j, unsigned int k) const;
-	double norme_vitesse(unsigned int pointi, unsigned int pointj, unsigned int pointk);
+	
+	
+	void iteration (double E = 0.1);
+	
+	
+	void resolution (double seuil = 2.2621843e-5, unsigned int max = 5000, bool verbeuse = false);
+	
+	
+	// Retourne le vecteur vitesse sous forme de tableau fixe (les trois coordonées)
+	std::array<double, 3> vitesse (unsigned int i, unsigned int j, unsigned int k) const;
+	
+	double norme_vitesse (unsigned int pointi, unsigned int pointj, unsigned int pointk);
 
-        void
-	std::vector<std::vector<std::vector<Potentiel>>> get_tableaudepotentiels();
+        void std::vector<std::vector<std::vector<Potentiel>>> get_tablO() const;
 		
 	private:
-	std::vector<std::vector<std::vector<Potentiel>>> potentiels3D;
-	 
+	
+	// Retourne la somme des carrés des normes des vecteurs laplaciens
+	double erreur();
 };

146b49135328aa3cf7c9665bdbf9600f30625599
clelialbrt <80390733+clelialbrt@users.noreply.github.com>
2021-05-29 13:17:49 +0200

    Update SupportADessin.h

--- SupportADessin.h
+++ SupportADessin.h
@@ -1,36 +1,23 @@
 #pragma once
-#include "Montagne.h"
-#include "Systeme.h"
-#include "Ciel.h"
-#include "SupportADessin.h"
 #include <iostream>
 
-// Super-classe qui est dédiée à la « visualisation » de la modélisation (mode texte ou graphique)
-// Classe abstraite dont hérite TextViewer (texte) et Open_vueGL (graphisme)
+
+class Montagne; // Prototypes des classes nécessaires à la déclaration de SupportADessin afin d'éviter une inclusion circulaire
+class Ciel;
+class Systeme;
 
 class SupportADessin 
 {
+	
+//--------------------------------------------------------------METHODES-------------------------------------------------------------//
+	
 	public:
+	virtual ~SupportADessin() = default; // demander à Sixtine de les commenter 
 	
-// On suppose ici que les supports ne seront ni copiés ni déplacés	
-virtual ~SupportADessin() = default;
+	virtual void dessine(Systeme const&) = 0;
+	
+	virtual void dessine(Montagne const&) = 0;
+	
+	virtual void dessine(Ciel const&) = 0;
 
-// Constructeur de la classe
-SupportADessin() {}
-
-// Méthodes virtuelles pures qui indiquent comment afficher les différents objets de la simulation
-// Ces méthodes sont pures car la classe ne sait pas encore comment afficher les elements dessinables
-
-virtual void dessine(Systeme const& a_dessiner) const = 0;
-virtual void dessine(Montagne const& a_dessiner) const = 0;
-virtual void dessine(Ciel const& a_dessiner) const = 0;
-	
-protected : 
-	
-// Permet d'éviter les copies de support à dessin, qui n'auraient pas de sens 
-	
-SupportADessin(SupportADessin const&) = delete;
-SupportADessin& operator=(SupportADessin const&) = delete;
-			
-	
 };

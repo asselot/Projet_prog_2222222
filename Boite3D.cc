028aa121075fb942cee38564be4b477c2bcf3663
clelialbrt <80390733+clelialbrt@users.noreply.github.com>
2021-05-29 12:59:43 +0200

    Update Boite3D.cc

--- Boite3D.cc
+++ Boite3D.cc
@@ -1,32 +1,37 @@
 #include <iostream>
+#include "Boite3D.h"
 #include <vector>
-#inlude "Boite3D.h"
+
+//--------------------------------------------------------------METHODES-------------------------------------------------------------//
 
 using namespace std;
 
-double Boite3D:: get_pas()
-{
-	return pas;
-}
-
-int Boite3D:: get_Nx()
+// Retourne le nombre de cubes selon x
+int Boite3D:: getNx() const 
 {
 	return Nx;
 }
 
-int Boite3D:: get_Ny()
+// Retourne le nombre de cubes selon y
+int Boite3D:: getNy() const 
 { 
 	return Ny;
 } 
 
-int Boite3D:: get_Nz()
+// Retourne le nombre de cubes selon z
+int Boite3D:: getNz() const 
 {
 	return Nz;
 }
 
+// Retourne le pas
+double Boite3D:: getpas() const 
+{
+	return pas;
+}
+
+// Retourne le tableau tridimensionnel
 std::vector<std::vector<std::vector<T>>> get_tablO() const 
 {
 	return tablO;
 }
-
-

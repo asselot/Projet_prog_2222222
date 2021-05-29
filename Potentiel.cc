cf92fe31fbd03ca6e57a2671c3c62fc7e7c726bb
clelialbrt <80390733+clelialbrt@users.noreply.github.com>
2021-05-29 13:16:12 +0200

    Update Potentiel.cc

--- Potentiel.cc
+++ Potentiel.cc
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <vector>
+#include "Potentiel.h"
+
+using namespace std;
+	
+//--------------------------------------------------------------METHODES-------------------------------------------------------------//
+
+// Affiche les attributs d'un potentiel
+void Potentiel:: affiche() 
+{
+	cout << poten << " " << laplacien << endl; 
+}
 

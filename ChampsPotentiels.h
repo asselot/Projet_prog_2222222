34294079d33a38749e5c628ba7d390dc31332ae1
clelialbrt <80390733+clelialbrt@users.noreply.github.com>
2021-05-29 13:02:01 +0200

    Update ChampsPotentiels.h

--- ChampsPotentiels.h
+++ ChampsPotentiels.h
@@ -11,45 +11,35 @@
 
 class ChampsPotentiels : public Boite3D<Potentiel>
 {
+	private:
+	//std::vector<std::vector<std::vector<Potentiel>>> potentiels3D; //mettre des template ici
+	
+	double erreur(); // Retourne la somme des carrés des normes des vecteurs laplaciens
+	
+	
+//--------------------------------------------------------------METHODES-------------------------------------------------------------//
+	
 	
 	public:
-
+	ChampsPotentiels(unsigned int nox = 30, unsigned int noy = 30, unsigned int noz = 30, double p = 1) : Boite3D(nox, noy, noz, p) {} // Constructeur d'un champ potentiel
 	
-	// Contructeur qui va construir par défaut une boîte 30x30x30 cubes de pas 1
-	ChampsPotentiels(unsigned int nox = 30, unsigned int noy = 30, unsigned int noz = 30, double p = 1) : Boite3D(nox, noy, noz, p) {}
+	void initialise(double vi, const Montagne& mont); // Initialise les potentiels du champ potentiel
 	
+	bool estauxbords(Potentiel pot) const; // Retourne true si un potentiel est au bord de la boîte
 	
-//--------------------------------------------------------------METHODES--------------------------------------------------------------------//
-
-        
-	// Initialise les potentiels de la boîte 
-	void initialise (double vi, const Montagne& mont);
+	void calcule_laplaciens(); // Calcule la valeur des laplaciens sur l'ensemble du champ à partir des potentiels 
 	
+	void iteration( double E = 0.1); // Aucune idée
 	
-	// Retourne true si un potentiel (vecteur) est au bord de la boîte
-	bool estauxbords (Potentiel pot) const;
+	void resolution ( double seuil = 2.2621843e-5, unsigned int max = 5000, bool verbeuse = false); //bruh
 	
+	std::array<double, 3> vitesse(unsigned int i, unsigned int j, unsigned int k) const; // Calcule la valeur de la vitesse du vent en un point donné
 	
-	// Calcule le laplacien en tout point intérieur de la boîte
-	void calcule_laplaciens();
+	double norme_vitesse(unsigned int pointi, unsigned int pointj, unsigned int pointk); // Norme de la vitesse
 	
+	void affiche(); // Affiche tous les potentiels du champ
 	
-	void iteration (double E = 0.1);
+	std::vector<std::vector<std::vector<Potentiel>>> get_tableaudepotentiels(); // Retourne le tableau de potentiels du champ
 	
-	
-	void resolution (double seuil = 2.2621843e-5, unsigned int max = 5000, bool verbeuse = false);
-	
-	
-	// Retourne le vecteur vitesse sous forme de tableau fixe (les trois coordonées)
-	std::array<double, 3> vitesse (unsigned int i, unsigned int j, unsigned int k) const;
-	
-	double norme_vitesse (unsigned int pointi, unsigned int pointj, unsigned int pointk);
-
-	// Méthode qui affiche le champ
-	virtual void affiche() const override; 
-		
-	private:
-	
-	// Retourne la somme des carrés des normes des vecteurs laplaciens
-	double erreur();
+	 
 };

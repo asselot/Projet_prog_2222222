a3e3ae7dfcb89029ceb4f41895eef2829508dfbc
clelialbrt <80390733+clelialbrt@users.noreply.github.com>
2021-05-29 13:02:22 +0200

    Update Ciel.cc

--- Ciel.cc
+++ Ciel.cc
@@ -1,23 +1,27 @@
 #include <iostream>
 #include "Ciel.h"
 
+using namespace std;
 
-// Méthode héritée de Dessinable
-void Ciel :: dessine_sur(SupportADessin a_dessiner) override
+//--------------------------------------------------------------METHODES-------------------------------------------------------------//
+
+// Demande à Sixtine
+void Ciel :: dessine_sur(SupportADessin& a_dessiner)
 { a_dessiner.dessine(*this); }
 
-
+// Calcul du déplacement du nuage pendant un temps donné à partir d'un point donné
 CubedAir Ciel:: precedente(double pas_temps, double x, double y, double z)
 {
-	double norme_vitesse_cubedair(sqrt(cubes_dair[x][y][z].vitesse_cubedair[0]*cubes_dair[x][y][z].vitesse_cubedair[0] + cubes_dair[x][y][z].vitesse_cubedair[1]*cubes_dair[x][y][z].vitesse_cubedair[1] + cubes_dair[x][y][z].vitesse_cubedair[2]*cubes_dair[x][y][z].vitesse_cubedair[2]));
+	//double norme_vitesse_cubedair(sqrt(tablO[x][y][z].vitesse_cubedair[0]*tablO[x][y][z].vitesse_cubedair[0] + tablO[x][y][z].vitesse_cubedair[1]*tablO[x][y][z].vitesse_cubedair[1] + tablO[x][y][z].vitesse_cubedair[2]*cubes_dair[x][y][z].vitesse_cubedair[2]));
 	
-  double deplacement(-pas_temps * norme_vitesse_cubedair);
-	
-  x += deplacement;
+	// Calcul du déplacement effectué et mise à jour des coordonnées
+	double deplacement(-pas_temps * tablO[x][y][z].vitesse_norme());
+	x += deplacement;
 	y += deplacement;
 	z += deplacement;
 	
-  if ((x > Nx) or (y > Ny) or (z > Nz))
+	// Si on se trouve au-delà de la boîte, l'état du nuage devient false (on ne se trouve plus dans le ciel)
+	if ((x > Nx) or (y > Ny) or (z > Nz))
 	{
 		tablO[x][y][z].etat == false;
 		return tablO[x][y][z];
@@ -27,9 +31,8 @@ CubedAir Ciel:: precedente(double pas_temps, double x, double y, double z)
 		return tablO[x][y][z];
 	}
 }
- //tester ces méthodes!!
 
-
+// Affiche tous les cubes d'air du ciel
 void Ciel:: affiche() override
 {
 	for (size_t x(0); x < get_Nx; ++x)

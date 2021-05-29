0a3940fb70aa520c6d46992cceaff91b22b555c7
clelialbrt <80390733+clelialbrt@users.noreply.github.com>
2021-05-29 13:16:44 +0200

    Update Potentiel.h

--- Potentiel.h
+++ Potentiel.h
@@ -1,18 +1,23 @@
 #pragma once 
 #include <iostream>
-#include "ChampsPotentiels.h"
 #include "Vecteur2D.h"
 
 class Potentiel
 {
-	friend class ChampsPotentiels;
-	public:
-	Potentiel() : poten(), laplacien() {}
-	void affiche();
-
 	protected:
-	Vecteur2D poten;
+	Vecteur2D poten; // Potentiel en lui-même 
+	
 	private:
-	Vecteur2D laplacien;
+	Vecteur2D laplacien; // Laplacien qui lui est associé
 
+	
+	friend class ChampsPotentiels; // Permet à la classe Champs Potentiels d'accéder à ses attributs protégés
+	
+//--------------------------------------------------------------METHODES-------------------------------------------------------------//
+	
+	public:
+	Potentiel() : poten(), laplacien() {} // Constructeur d'un potentiel
+	
+	void affiche(); // Affiche les attributs d'un potentiel
+	
 };

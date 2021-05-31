e8d4883a071c0d2eda54e2d763228ab1fbe52550
asselot <80883289+asselot@users.noreply.github.com>
2021-05-31 11:55:38 +0200

    Update ChampsPotentiels.h

--- ChampsPotentiels.h
+++ ChampsPotentiels.h
@@ -12,15 +12,17 @@
 class ChampsPotentiels : public Boite3D<Potentiel>
 {
 	private:
-	//std::vector<std::vector<std::vector<Potentiel>>> potentiels3D; //mettre des template ici
 	
 	double erreur(); // Retourne la somme des carrés des normes des vecteurs laplaciens
+	
+	void iteration( double E = 0.1); // Aucune idée
 	
 	
 //--------------------------------------------------------------METHODES-------------------------------------------------------------//
 	
 	
 	public:
+	
 	ChampsPotentiels(unsigned int nox = 30, unsigned int noy = 30, unsigned int noz = 30, double p = 1) : Boite3D(nox, noy, noz, p) {} // Constructeur d'un champ potentiel
 	
 	void initialise(double vi, const Montagne& mont); // Initialise les potentiels du champ potentiel
@@ -29,17 +31,10 @@ class ChampsPotentiels : public Boite3D<Potentiel>
 	
 	void calcule_laplaciens(); // Calcule la valeur des laplaciens sur l'ensemble du champ à partir des potentiels 
 	
-	void iteration( double E = 0.1); // Aucune idée
-	
 	void resolution ( double seuil = 2.2621843e-5, unsigned int max = 5000, bool verbeuse = false); //bruh
 	
 	std::array<double, 3> vitesse(unsigned int i, unsigned int j, unsigned int k) const; // Calcule la valeur de la vitesse du vent en un point donné
 	
-	double norme_vitesse(unsigned int pointi, unsigned int pointj, unsigned int pointk); // Norme de la vitesse
+	double norme_vitesse(unsigned int pointi, unsigned int pointj, unsigned int pointk) const; // Norme de la vitesse
 	
-	void affiche(); // Affiche tous les potentiels du champ
-	
-	std::vector<std::vector<std::vector<Potentiel>>> get_tableaudepotentiels(); // Retourne le tableau de potentiels du champ
-	
-	 
 };

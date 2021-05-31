2f44e23990fc32a8751dec968ab633bbb0e1c2db
asselot <80883289+asselot@users.noreply.github.com>
2021-05-31 12:09:28 +0200

    Update Boite3D.cc

--- Boite3D.cc
+++ Boite3D.cc
@@ -35,3 +35,44 @@ std::vector<std::vector<std::vector<T>>> get_tablO() const
 {
 	return tablO;
 }
+
+// Affiche les paramètres de la boîte
+void Boite3D :: affiche() const
+{
+	for (unsigned int x(0); x < Nx; ++x)
+	{
+		for (unsigned int y(0); y < Ny; ++y)
+		{
+			for (unsigned int z(0); z < Nz; ++z)
+			{
+				
+					tablO[x][y][z].affiche();
+				
+			}
+		}
+	}
+	
+}
+
+ostream& operator << (ostream& sortie, Boite3D& b)
+{
+	sortie << "Dimension de la boîte :" << endl;
+	sortie << "Nx: " << get_Nx << " Ny : " << get_Ny << " Nz : " << get_Nz << endl;
+	sortie << "Le pas des cubes :" << get_pas << endl;
+	
+	for (unsigned int x(0); x < get_Nx; ++x)
+	{
+		for (unsigned int y(0); y < get_Ny; ++y)
+		{
+			for (unsigned int z(0); z < get_Nz; ++z)
+			{
+				
+					tablO[x][y][z].affiche();
+				
+			}
+		}
+	}
+	
+}
+}
+	

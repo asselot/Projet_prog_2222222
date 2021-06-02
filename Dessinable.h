9c16bcfd755a386316e0294f90b188aa2d44b591
asselot <80883289+asselot@users.noreply.github.com>
2021-06-02 09:50:11 +0200

    Update Dessinable.h

--- Dessinable.h
+++ Dessinable.h
@@ -6,8 +6,9 @@
 class Dessinable //  permet de classer les objets qui sont dessinable tout les objets necessaure pour dessiner 
 {
 	public :
-	
+    virtual ~Dessinable() =0;
 	virtual void dessine_sur(SupportADessin& a_dessiner) = 0; // Méthode virtuelle pure permettant de faire jsp quoi
 	
 	
 };
+

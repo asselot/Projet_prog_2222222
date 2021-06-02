c41ab1f44b17232347756ac19689b58da5736c30
asselot <80883289+asselot@users.noreply.github.com>
2021-06-02 09:42:27 +0200

    Update Systeme.cc

--- Systeme.cc
+++ Systeme.cc
@@ -1,31 +1,39 @@
+
 #include <iostream>
 #include "Systeme.h"
 
 //--------------------------------------------------------------METHODES-------------------------------------------------------------//
 
 // Fait appel aux méthodes affiche() des différents composants du système
-void Systeme :: affiche (ostream& sortie) const
+std::ostream& Systeme :: affiche (std::ostream& sortie) const
 {
-	sortie << " Montagnes : " << &ptr_hill << endl;
-	sortie << " Champs potentiels : " << &ptr_champs << endl;
-	sortie << " Ciel : " << &ptr_ciel << endl;
+    sortie << " Montagnes : " << *ptr_hill << std::endl;
+    sortie << " Champs potentiels : " << *ptr_champs << std::endl;
+    sortie << " Ciel : " << *ptr_ciel << std::endl;
+    return sortie;
+}
+
+Systeme :: Systeme (Systeme const& S) {
+    ptr_champs = new ChampsPotentiels(*S.ptr_champs);
+    ptr_ciel = new Ciel (*S.ptr_ciel);
+    ptr_hill = new Montagne (*S.ptr_hill);
 }
 
 // Démarre modélisation du système : créer un champ de potentiel et un ciel, dessine et fait évoluer le système 
-virtual void Systeme :: demarre (SupportADessin& a_dessiner)
+void Systeme :: demarre (SupportADessin& a_dessiner)
 {
-	ptr_champs ->resolution();
-	ptr_ciel = new Ciel(&ptr_champs); // permet de débeuguer 
+    ptr_champs->resolution();
+    ptr_ciel = new Ciel(*ptr_champs); // permet de débeuguer
 	dessine_sur(a_dessiner);
 	evolue(a_dessiner);
 }
 
 // Méthode dessine_sur() héritée de Dessinable
-virtual void Systeme :: dessine_sur(SupportADessin& a_dessiner) override
-{ a_dessiner->dessine(*this); }
+void Systeme :: dessine_sur(SupportADessin& a_dessiner)
+{ a_dessiner.dessine(*this); }
 
 // Fait évoluer le système (surtout nuage?)
-virtual void Systeme ::  evolue (SupportADessin& a_dessiner)
+void Systeme ::  evolue (SupportADessin& a_dessiner)
 {
 	// pas besoin d'itération sur cubeair
 	dessine_sur(a_dessiner);	
@@ -35,5 +43,5 @@ virtual void Systeme ::  evolue (SupportADessin& a_dessiner)
 
 std::ostream& operator<<(std::ostream& sortie, Systeme const& Sys)
 {
-	Sys.affiche(sortie);
+     return Sys.affiche(sortie);
 }

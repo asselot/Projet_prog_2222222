fd9ce40b3db64eb0badfea31c206089605e19739
clelialbrt <80390733+clelialbrt@users.noreply.github.com>
2021-06-03 15:41:49 +0200

    Update constantes.cc

--- constantes.cc
+++ constantes.cc
@@ -1,15 +1,14 @@
 #include "constantes.h"
 
-//Fichier contenant toutes les constantes physiques qui sont nécessaires dans certaines classes
 
-	extern const double Physique::g(9.81);                                // Accélération gravitationnelle
-        extern const double Physique::vinfini(20.0)                            // Vitesse aux bords
-	extern const double Physique::Tinfini(284.5);                         // Température aux bords
-	extern const double Physique::Pinfini(101325);                        // Pression de l'air au bord
-	extern const double Physique::Pref(101325);                           // Pression de référence
-	extern const double Physique::tau(8.10e-3);                           // Taux d'humidité
-	extern const double Physique::Meau(0.0180153);                        // Masse molaire eau
-	extern const double Physique::Mair_sec(0.02896);                      // Masse molaire air sec
-	extern const double Physique::R(8.3144621);                           // Constante des gaz parfait
-	extern const double Physique::hinfini((7/2)*(R/Mair_sec)*Tinfini);    // Enthalpie aux bords
-	extern const double Physique::cte(0.5*vinfini*vinfini + hinfini);     // Constante de Bernouilli
+	extern const double Physique::g(9.81); // Accélération gravitationnelle 
+	extern const double Physique::vinfini(20.0); // Vitesse aux bords de la plaine
+	extern const double Physique::Tinfini(284.5); // Température aux bords de la plaine 
+	extern const double Physique::Pinfini(101325.0); // Pression aux bords de la plaine 
+	extern const double Physique::Pref(101325.0); // Pression de référence 
+	extern const double Physique::tau(0.00810); // Taux d'humidité
+	extern const double Physique::Meau(0.0180153); // Masse molaire de l'eau
+	extern const double Physique::Mair_sec(0.02896); // Masse molaire de l'air sec
+	extern const double Physique::R(8.3144621); // Constante des gaz parfaits 
+	extern const double Physique::hinfini((7.0/2.0)*(R/Mair_sec)*Tinfini); // Entropie aux bords de la plaine 
+	extern const double Physique::cte(0.5*vinfini*vinfini + hinfini); // Constante de Bernouilli

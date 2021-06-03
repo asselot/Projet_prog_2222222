57ff8a875498fbf56910e0bfb5475a0def9f5662
asselot <80883289+asselot@users.noreply.github.com>
2021-06-03 14:35:56 +0200

    Update Systeme.h

--- Systeme.h
+++ Systeme.h
@@ -3,31 +3,27 @@
 #include "Montagne.h"
 #include "Ciel.h"
 #include "ChampsPotentiels.h"
+#include "constantes.h"
+
 
 // Représente ce qui forme un système physique
 
 class Systeme : public Dessinable
 
 {
-	
+
+private :
+
+// Interdiction de la copie: conceptuellement problématique
+Systeme(Systeme& systeme) = delete;
+
 public :
 
+// Constructeur à partir d'une montagne déjà initialisée et des dimensions de la boîte
+Systeme (int const&  nx, int const&  ny, int const&  nz, double const&  pas,  Montagne const& M);
 
-Systeme (Systeme const& S);
-
-// Constructeur de copie nécessaire 
-Systeme (Montagne& M, Ciel& C, ChampsPotentiels& Ch)
-: ptr_champs(new ChampsPotentiels(Ch)), ptr_ciel(new Ciel(C)), ptr_hill(new Montagne(M)) {}
-
-// Destructeur de la classe : delete chaque attributs car ce sont des pointeurs
-~Systeme()
-{
-    ptr_champs->~ChampsPotentiels();
-    delete ptr_champs;
-    ptr_champs = nullptr;
-    delete ptr_ciel;
-    delete ptr_hill;
-}
+// Destructeur de la classe
+~Systeme() = default;
 
 //--------------------------------------------------------------METHODES-------------------------------------------------------------//
 
@@ -37,25 +33,39 @@ std::ostream& affiche (std::ostream& sortie) const;
 // Démarre modélisation du système : créer un champ de potentiel et un ciel, dessine et fait évoluer le système 
 void demarre (SupportADessin& a_dessiner);
 
-// Méthode dessine_sur() héritée de Dessinable
-virtual void dessine_sur(SupportADessin& a_dessiner) override;
-
-private : 
-
 // Fait évoluer le système (surtout nuage?)
 void evolue (SupportADessin& a_dessiner);
 
-//--------------------------------------------------------------ATTRIBUTS-------------------------------------------------------------//
+// Méthode dessine_sur() héritée de Dessinable
+virtual void dessine_sur(SupportADessin& a_dessiner) override;
 
+// En public pour le graphisme
+
+
+ChampsPotentiels champs;
+Ciel ciel;
+Montagne montagne;
+
+//--------------------------------------------------------------ATTRIBUTS-------------------------------------------------------------//
+/*
 ChampsPotentiels* ptr_champs;
 Ciel* ptr_ciel;
 Montagne* ptr_hill;
-
+*/
 //--------------------------------------------------------------OPERATEUR-------------------------------------------------------------//
 
-	
-// Interdiction de la copie: conceptuellement problématique
-Systeme(Systeme& systeme) = delete; 
+// Constructeur de copie nécessaire
+/*Systeme (Montagne& M, Ciel& C, ChampsPotentiels& Ch)
+: ptr_champs(new ChampsPotentiels(Ch)), ptr_ciel(new Ciel(C)), ptr_hill(new Montagne(M)) {}*/
+
+//Systeme(Montagne& M, ChampsPotentiels& Ch, Ciel& C);
+
+// Destructeur de la classe : delete chaque attributs car ce sont des pointeurs
+
+
+
+
 };
 
 std::ostream& operator<<(std::ostream& sortie, Systeme const& Sys);
+

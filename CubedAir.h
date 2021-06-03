cc5473217e29881c6cf659951fd5b8966c90ed58
clelialbrt <80390733+clelialbrt@users.noreply.github.com>
2021-06-03 10:13:32 +0200

    Update CubedAir.h

--- CubedAir.h
+++ CubedAir.h
@@ -1,55 +1,43 @@
 #pragma once
 #include <iostream>
-#pragma once
-#include <iostream>
+#include <array> 
 #include "Boite3D.h"
 #include "ChampsPotentiels.h"
 #include "constantes.h"
 
 class CubedAir
 {	
-	private:
-	double enthalpie; // Enthalpie d'un cube
-	
-	double temperature; // Température d'un cube
-	
-	double pression; // Pression d'un cube 
-	
-	double pression_partielle; // Pression de l'eau dans un cube 
-	
-	double pression_vapeur_saturante; // Pression de la rosée dans un cube 
-	
 	protected:
-	bool etat; // Etat d'un cube (nuage ou non)
-	
 	std::array<double, 3> vitesse_cubedair; // Vitesse d'un cube 
 	
 	//--------------------------------------------------------------METHODES-------------------------------------------------------------//
 	
 	friend class Ciel;
 	public:
-	CubedAir() : enthalpie(0), temperature(0), pression(0), pression_partielle(0), pression_vapeur_saturante(0), etat(false) {} // Constructeur d'un cube d'air 
 	
-	void set_enthalpie(unsigned int pointx, unsigned int pointy, unsigned int pointz); // Calcule l'enthalpie en un point donné
+	CubedAir() { vitesse_cubedair[0] = 0; vitesse_cubedair[1] = 0; vitesse_cubedair[2] = 0; }
 	
-	void set_temperature(unsigned int px, unsigned int py, unsigned int pz); // Calcule la température en un point donné
+	CubedAir(std::array<double, 3> vit_init) : vitesse_cubedair(vit_init) {} 
 	
-	void set_pression(unsigned int px, unsigned int py, unsigned int pz); // Calcule la pression en un point donné
+	double enthalpie(unsigned int pointx, unsigned int pointy, unsigned int pointz) const; // Calcule l'enthalpie en un point donné
 	
-	void set_pression_partielle(unsigned int px, unsigned int py, unsigned int pz); // Calcule la pression de l'eau (Peau)
+	double temperature(unsigned int px, unsigned int py, unsigned int pz) const; // Calcule la température en un point donné
 	
-	void set_pression_vapeur_saturante(unsigned int px, unsigned int py, unsigned int pz); // Calcule la pression de la rosée (Prosée)
+	double pression(unsigned int px, unsigned int py, unsigned int pz) const; // Calcule la pression en un point donné
 	
-	void set_etat(unsigned int px, unsigned int py, unsigned int pz); // Détermine l'état du cube d'air, elle retourne true si il est sous forme de nuage 
+	double pression_partielle(unsigned int px, unsigned int py, unsigned int pz) const; // Calcule la pression de l'eau (Peau)
 	
-	bool get_etat() const; // Retourne l'état du cube d'air (nuage ou non)
+	double pression_vapeur_saturante(unsigned int px, unsigned int py, unsigned int pz) const; // Calcule la pression de la rosée (Prosée)
+	
+	bool etat(unsigned int px, unsigned int py, unsigned int pz) const; // Détermine l'état du cube d'air, elle retourne true si il est sous forme de nuage 
 	
 	void set_vitesse(double coordx, double coordy, double coordz); // Associe les valeurs données aux coordonnées de la vitesse du cube 
 	
-    void affiche(std :: ostream& sortie) const; // Affiche les différents attributs du cube d'air
+    void affiche(std::ostream& sortie, unsigned int px, unsigned int py, unsigned int pz) const; // Affiche les différents attributs du cube d'air
 	
 	double vitesse_norme() const; // Calcule la norme de la vitesse d'un cube d'air 
 	
-	void affiche_etat() const; // Affiche 1 si le cube est un nuage, 0 sinon
+	void affiche_etat(unsigned int px, unsigned int py, unsigned int pz) const; // Affiche 1 si le cube est un nuage, 0 sinon
 	
+	//ENTHALPIE ET CSTE DE BERNOUILLI CHANGENT 
 };

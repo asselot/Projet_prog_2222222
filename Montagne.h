5ef98ba2fbdf780102760190b781bb0839ab6ce9
clelialbrt <80390733+clelialbrt@users.noreply.github.com>
2021-06-03 10:14:11 +0200

    Update Montagne.h

--- Montagne.h
+++ Montagne.h
@@ -5,7 +5,6 @@
 #include <iostream>
 #include "Dessinable.h"
 #include "SupportADessin.h"
-#include "ChampsPotentiels.h"
 
 class Montagne : public Dessinable 
 {
@@ -21,30 +20,26 @@ class Montagne : public Dessinable
 	
 	double ey; // Etalement en y de la montagne 
 	
-
-	
 //--------------------------------------------------------------METHODES-------------------------------------------------------------//
 
 	public: 
 	Montagne(double x, double y, double h, double ox, double oy) : x0(x), y0(y), H(h), ex(ox), ey(oy) {} // Constructeur d'une montagne avec des valeurs données 
 	
-	Montagne() : x0(0), y0(0), H(0), ex(0), ey(0) {} // Constructeur d'une Montagne initialisant tout à 0
+	virtual double altitude(double const& a, double const& b) const; // Méthode virtuelle qui retourne le maximum des altitudes des montagnes simples et des sous-chaînes qui composent la chaîne 
 	
-	virtual double altitude(double a, double b) const; // Méthode virtuelle qui retourne le maximum des altitudes des montagnes simples et des sous-chaînes qui composent la chaîne 
-	
-	virtual void set_montagne(double x, double y, double h, double ox, double oy); // Méthode permettant de modifier les attributs d'une montagne avec les valeurs passées en arguments
+	virtual void set_montagne(double const& x, double const& y, double const& h, double const& ox, double const& oy); // Méthode permettant de modifier les attributs d'une montagne avec les valeurs passées en arguments
 			
 	void dessine_sur(SupportADessin &a_dessiner) override ; // Méthode dessine_sur redéfinie
 	
-	double get_x0();
+	double get_x0() const;
 	
-	double get_y0();
+	double get_y0() const;
 	
-	double get_H();
+	double get_H() const;
 	
-	double get_ex();
+	double get_ex() const;
 	
-	double get_ey();
+	double get_ey() const;
 	
 };
 
@@ -54,19 +49,19 @@ class Montagne : public Dessinable
 class ChaineDeMontagnes : public Montagne
 {
 	private:
-	unsigned int nombre_montagnes; // Nombre de montagnes simples qui composent la chaîne
+	/*unsigned int nombre_montagnes; // Nombre de montagnes simples qui composent la chaîne
 	
-	unsigned int nombre_chaines; // Nombre de sous-chaînes qui composent la chaîne 
+	unsigned int nombre_chaines; // Nombre de sous-chaînes qui composent la chaîne */
 	
 	std::vector<Montagne> montagnes_simples; // Tableau de montagnes simples
 	
-	std::vector<ChaineDeMontagnes> chaines_simples; // Tableau de chaînes simples
+	//std::vector<ChaineDeMontagnes> chaines_simples; // Tableau de chaînes simples
 	
 //--------------------------------------------------------------METHODES-------------------------------------------------------------//
 
 	public: 
 
-	ChaineDeMontagnes(double x, double y, double h, double ox, double oy, const unsigned int &nombre) : Montagne(x, y, h, ox, oy), nombre_montagnes(nombre) // Construit une chaîne composée uniquement de la même montagne simple 
+	/*ChaineDeMontagnes(double x, double y, double h, double ox, double oy, const unsigned int &nombre) : Montagne(x, y, h, ox, oy), nombre_montagnes(nombre) // Construit une chaîne composée uniquement de la même montagne simple 
 	{
 		Montagne mon(x, y, h, ox, oy);
 		for (size_t i(0); i < nombre_montagnes; ++i)
@@ -81,39 +76,26 @@ class ChaineDeMontagnes : public Montagne
 		{
 			chaines_simples.push_back(chaine_a_mettre);
 		}
-	}
+	}*/
 	
-	ChaineDeMontagnes() : Montagne(), nombre_montagnes(0), nombre_chaines(0) // Construit une chaine composée d'une seule montagne et d'une seule chaîne, toutes deux vides
-	{
-		std::vector<Montagne> monts; 
-		std::vector<ChaineDeMontagnes> chais;
-		montagnes_simples.push_back(monts);
-		chaines_simples.push_back(chais);
-	}
-	
-	ChaineDeMontagnes(const unsigned int &nombre, const unsigned int &nombre2) : Montagne(0, 0, 0, 0, 0), nombre_montagnes(nombre), nombre_chaines(nombre2) // Constructeur ajoutant à la chaîne autant de montagnes simples et de montagnes vides que souhaité
-	{
-		Montagne mon;
-		for (auto i : montagnes_simples)
-		{
-			montagnes_simples.push_back(mon);
-		}
-		ChaineDeMontagnes chai; 
-		for (auto j : chaines_simples)
-		{
-			chaines_simples.push_back(chai); 
-		}
-	}
-	
-	int get_nbre_montagnes(); // Retourne le nombre de montagnes simples qui composent la chaîne 
-	
-	int get_nbre_chaines(); // Retourne le nombre de sous-chaînes qui composent la chaîne 
-	
-	void set_montagne(double x, double y, double h, double ox, double oy, const unsigned int &indice); // Méthode donnant aux attributs de la i-ème montagne simple de la chaîne les valeurs passées en arguments
-	
-	void set_montagne(ChaineDeMontagnes chaine, const unsigned int &indice); // Méthode remplaçant la i-ème sous-chaîne de la chaîne par celle passée en argument 
 
-	double altitude(double a, double b) const override; // Redéfinition de la méthode virtuelle qui retourne le maximum des altitudes des montagnes simples et des sous-chaînes qui composent la chaîne 
+	
+	
+	ChaineDeMontagnes(const Montagne& mont) : Montagne(mont)
+	{
+		montagnes_simples.push_back(mont);
+	}
+	
+	/*ChaineDeMontagnes(const ChaineDeMontagnes& chaine) : Montagne(0, 0, 0, 0, 0)
+	{
+		chaines_simples.push_back(chaine);
+	}*/
+	
+	void set_montagne(Montagne const& montagne); // Méthode donnant aux attributs de la i-ème montagne simple de la chaîne les valeurs passées en arguments
+	
+	//void set_montagne(ChaineDeMontagnes const& chaine); // Méthode remplaçant la i-ème sous-chaîne de la chaîne par celle passée en argument 
+
+	double altitude(double const& a, double const& b) const override; // Redéfinition de la méthode virtuelle qui retourne le maximum des altitudes des montagnes simples et des sous-chaînes qui composent la chaîne 
 	
 	
 

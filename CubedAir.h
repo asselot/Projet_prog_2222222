8c5f8e0aa2c3307c7b8df28bae7157a151d41d02
asselot <80883289+asselot@users.noreply.github.com>
2021-06-03 19:01:03 +0200

    Update CubedAir.h

--- CubedAir.h
+++ CubedAir.h
@@ -1,45 +1,45 @@
 #pragma once
 #include <iostream>
-#include <array> 
 #include "Boite3D.h"
 #include "ChampsPotentiels.h"
 #include "constantes.h"
 
+
 class CubedAir
-{	
-	protected:
-	std::array<double, 3> vitesse_cubedair; // Vitesse d'un cube 
-	
-	//--------------------------------------------------------------METHODES-------------------------------------------------------------//
-	
-	friend class Ciel;
-	public:
-	
-	CubedAir() { vitesse_cubedair[0] = 0; vitesse_cubedair[1] = 0; vitesse_cubedair[2] = 0; }
-	
-	CubedAir(std::array<double, 3> vit_init) : vitesse_cubedair(vit_init) {} 
-	
-	double enthalpie(unsigned int pz, double pas) const; // Calcule l'enthalpie en un point donné
-	
-	double temperature(unsigned int pz, double pas) const; // Calcule la température en un point donné
-	
-	double pression(unsigned int pz, double pas) const; // Calcule la pression en un point donné
-	
-	double pression_partielle(unsigned int pz, double pas) const; // Calcule la pression de l'eau (Peau)
-	
-	double pression_vapeur_saturante(unsigned int pz, double pas) const; // Calcule la pression de la rosée (Prosée)
-	
-	bool etat(unsigned int pz, double pas) const; // Détermine l'état du cube d'air, elle retourne true si il est sous forme de nuage 
-	
-	void set_vitesse(double coordx, double coordy, double coordz); // Associe les valeurs données aux coordonnées de la vitesse du cube 
-	
+{
+    protected:
+    std::array<double, 3> vitesse_cubedair; // Vitesse d'un cube
+
+    //--------------------------------------------------------------METHODES-------------------------------------------------------------//
+
+    public:
+
+    // Constructeur qui met la vitesse des cubes à 0
+    CubedAir();
+
+    // Constructeur à partir de la vitesse
+    CubedAir(std::array<double, 3> vit_init);
+
+    double enthalpie(unsigned int pz, double pas) const; // Calcule l'enthalpie en un point donné
+
+    double temperature(unsigned int pz, double pas) const; // Calcule la température en un point donné
+
+    double pression(unsigned int pz, double pas) const; // Calcule la pression en un point donné
+
+    double pression_partielle(unsigned int pz, double pas) const; // Calcule la pression de l'eau (Peau)
+
+    double pression_vapeur_saturante(unsigned int pz, double pas) const; // Calcule la pression de la rosée (Prosée)
+
+    bool etat(unsigned int pz, double pas) const; // Détermine l'état du cube d'air, elle retourne true si il est sous forme de nuage
+
+    void set_vitesse(double coordx, double coordy, double coordz); // Associe les valeurs données aux coordonnées de la vitesse du cube
+
     void affiche(std::ostream& sortie, unsigned int pz, double pas) const; // Affiche les différents attributs du cube d'air
-	
-	double vitesse_norme() const; // Calcule la norme de la vitesse d'un cube d'air 
-	
-	double v2() const;
-	
-	void affiche_etat(unsigned int pz, double pas) const; // Affiche 1 si le cube est un nuage, 0 sinon
-	
-	//ENTHALPIE ET CSTE DE BERNOUILLI CHANGENT 
+
+    double vitesse_norme() const; // Calcule la norme de la vitesse d'un cube d'air
+
+    double v2() const;
+
+    void affiche_etat(unsigned int pz, double pas) const; // Affiche 1 si le cube est un nuage, 0 sinon
+
 };

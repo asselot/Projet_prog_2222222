8acf25d3f38fdda64bc49d5e365401b7dece2c9f
asselot <80883289+asselot@users.noreply.github.com>
2021-06-03 19:01:36 +0200

    Update Vecteur2D.h

--- Vecteur2D.h
+++ Vecteur2D.h
@@ -1,35 +1,27 @@
 #pragma once
 #include <iostream>
 #include <ostream>
-#include <cmath>
+#include <cmath> 
 
 class Vecteur2D
 {
     protected:
+
     double coord_x; // coordonnée x
     double coord_y; // coordonnée y
-	
-    friend std::ostream& operator<<(std::ostream& sortie, Vecteur2D const& vect); // ?
-    friend Vecteur2D operator * (const double x, const Vecteur2D& V);
-    friend Vecteur2D operator * (const Vecteur2D& V, const double x);
-    
-    
+
     // Permet à la classe ChampsPotentiels d'utiliser les attributs d'un vecteurs ... à voir
     friend class ChampsPotentiels;
-	
 	
     public:
 	
    
-    // constructeur par défaut au vecteur nul	
-    Vecteur2D (const double x=0, const double y=0) 
-    : coord_x(x), coord_y(y)
-    {}
-    
-   /* Vecteur2D( Vecteur2D const& autre) /// le constructeur de copie par défaut par défaut suffit amplement
-    :coord_x( autre.coord_x), coord_y(autre.coord_y)
-    {}*/
-	
+    // Constructeur par défaut au vecteur nul
+    Vecteur2D (int x=0, int y=0);
+
+   // le constructeur de copie par défaut
+   Vecteur2D( Vecteur2D const& autre);
+
 //--------------------------------------------------------------OPERATEURS INTERNES-------------------------------------------------------------//
 
     
@@ -38,24 +30,23 @@ class Vecteur2D
 
 	
     // - opérateur interne - Retourne l'addition au vecteur du vecteur "V" 
-    Vecteur2D& operator += (const Vecteur2D& V); 
-
+    Vecteur2D& operator += (const Vecteur2D& V);
+	
     
     // - opérateur interne - Retourne la soustraction au vecteur du vecteur "V" 
-    Vecteur2D& operator -= (const Vecteur2D& V);  
+    Vecteur2D& operator -= (const Vecteur2D& V);
 	
         
     // - opérateur interne - Retourne le vecteur multiplié par x
-	Vecteur2D& operator *= (const double& x);
+    Vecteur2D& operator *= (const double& x);
 	
-    
-    // Retourne l'opposé du vecteur
-    //Vecteur2D& operator op();  //changer le nom en operateur connu 
 	
     // - opérateur interne - Retourne le vecteur unitaire associé à la direction du vecteur initial
     Vecteur2D& operator ~ ();
 
-	Vecteur2D& operator = (Vecteur2D const& V);
+    // - opérateur interne - Affectation
+    Vecteur2D& operator = (Vecteur2D const& V);
+
 	
 //--------------------------------------------------------------METHODES-------------------------------------------------------------//
 
@@ -70,26 +61,29 @@ class Vecteur2D
 	
     //Retourne la norme du vecteur
     double norme() const;
-	
-	Vecteur2D oppose() const;
-	
-    /*// Vecteur unitaire   
-    Vecteur2D unitaire() const;*/ 
     
+       
+    // Retourne l'opposé du vecteur
+    Vecteur2D oppose() const;
+	
+    // getteurs - permet d'accéder aux attributs privés
     double get_x() const;
-    
     double get_y() const;
-    
+
+    // Méthode qui affiche les paramètres du veteur
+    void afficher(std::ostream &sortie) const;
 };
 
 //--------------------------------------------------------------OPERATEURS EXTERNES-------------------------------------------------------------//
-		
-Vecteur2D operator + (Vecteur2D v1, const Vecteur2D& v2);  // operateur externe +
-		
-Vecteur2D operator - (Vecteur2D v1, const Vecteur2D& v2);  // operateur externe -
-		
-Vecteur2D operator * (const double x, const Vecteur2D& V); // operateur externe *
 
-Vecteur2D operator * (const Vecteur2D& V, const double x); // assure commutativité de la multiplication par un scalaire
+
+		
+Vecteur2D& operator + (Vecteur2D v1, Vecteur2D const& v2);  // operateur externe +
+		
+Vecteur2D& operator - (Vecteur2D v1,  Vecteur2D const& v2);  // operateur externe -
+		
+Vecteur2D& operator * (double const& x, Vecteur2D& V); // operateur externe *
+
+Vecteur2D& operator * (Vecteur2D& V, double const& x); // assure commutativité de la multiplication par un scalaire
 
 std::ostream& operator<<(std::ostream& sortie, Vecteur2D const& V); // permet d'afficher un vecteur par surcharge de <<

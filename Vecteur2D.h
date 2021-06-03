b930cb3e28eb2c1a62f595fc8bf905fcfd64ac41
clelialbrt <80390733+clelialbrt@users.noreply.github.com>
2021-06-03 10:09:04 +0200

    Update Vecteur2D.h

--- Vecteur2D.h
+++ Vecteur2D.h
@@ -1,14 +1,18 @@
 #pragma once
 #include <iostream>
 #include <ostream>
-#include <cmath> 
+#include <cmath>
 
 class Vecteur2D
 {
     protected:
     double coord_x; // coordonnée x
     double coord_y; // coordonnée y
-
+	
+    friend std::ostream& operator<<(std::ostream& sortie, Vecteur2D const& vect); // ?
+    friend Vecteur2D operator * (const double x, const Vecteur2D& V);
+    friend Vecteur2D operator * (const Vecteur2D& V, const double x);
+    
     
     // Permet à la classe ChampsPotentiels d'utiliser les attributs d'un vecteurs ... à voir
     friend class ChampsPotentiels;
@@ -18,7 +22,7 @@ class Vecteur2D
 	
    
     // constructeur par défaut au vecteur nul	
-    Vecteur2D (int x=0, int y=0) 
+    Vecteur2D (const double x=0, const double y=0) 
     : coord_x(x), coord_y(y)
     {}
     
@@ -30,24 +34,28 @@ class Vecteur2D
 
     
     //Retourne false si au moins l'une des cordonnées de vector n'est pas égale à celle du vecteur comparé (à la précision près)	
-    bool operator == (Vecteur2D const& V) const;
+    bool operator == (const Vecteur2D& V) const;
 
 	
     // - opérateur interne - Retourne l'addition au vecteur du vecteur "V" 
-    Vecteur2D& operator += (Vecteur2D const& V); 
-	
+    Vecteur2D& operator += (const Vecteur2D& V); 
+
     
     // - opérateur interne - Retourne la soustraction au vecteur du vecteur "V" 
-    Vecteur2D& operator -= (Vecteur2D const& V);  
+    Vecteur2D& operator -= (const Vecteur2D& V);  
 	
         
     // - opérateur interne - Retourne le vecteur multiplié par x
-    Vecteur2D& operator *= (double const& x);  
+	Vecteur2D& operator *= (const double& x);
 	
+    
+    // Retourne l'opposé du vecteur
+    //Vecteur2D& operator op();  //changer le nom en operateur connu 
 	
     // - opérateur interne - Retourne le vecteur unitaire associé à la direction du vecteur initial
     Vecteur2D& operator ~ ();
 
+	Vecteur2D& operator = (Vecteur2D const& V);
 	
 //--------------------------------------------------------------METHODES-------------------------------------------------------------//
 
@@ -62,27 +70,26 @@ class Vecteur2D
 	
     //Retourne la norme du vecteur
     double norme() const;
+	
+	Vecteur2D oppose() const;
+	
+    /*// Vecteur unitaire   
+    Vecteur2D unitaire() const;*/ 
     
-       
-    // Retourne l'opposé du vecteur
-    Vecteur2D oppose () const; 
-	
-	
-    // getteurs - permet d'accéder aux attributs privés
     double get_x() const;
+    
     double get_y() const;
+    
 };
 
 //--------------------------------------------------------------OPERATEURS EXTERNES-------------------------------------------------------------//
-
-
 		
-Vecteur2D& operator + (Vecteur2D v1, const Vecteur2D& v2);  // operateur externe +
+Vecteur2D operator + (Vecteur2D v1, const Vecteur2D& v2);  // operateur externe +
 		
-Vecteur2D& operator - (Vecteur2D v1, const Vecteur2D& v2);  // operateur externe -
+Vecteur2D operator - (Vecteur2D v1, const Vecteur2D& v2);  // operateur externe -
 		
-Vecteur2D& operator * (double const& x, Vecteur2D& V); // operateur externe *
+Vecteur2D operator * (const double x, const Vecteur2D& V); // operateur externe *
 
-Vecteur2D& operator * (Vecteur2D& V, double const& x); // assure commutativité de la multiplication par un scalaire
+Vecteur2D operator * (const Vecteur2D& V, const double x); // assure commutativité de la multiplication par un scalaire
 
 std::ostream& operator<<(std::ostream& sortie, Vecteur2D const& V); // permet d'afficher un vecteur par surcharge de <<

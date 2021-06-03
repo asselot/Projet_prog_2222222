990968eea803cd961c15b4f1ca80a6c87b1c24a4
asselot <80883289+asselot@users.noreply.github.com>
2021-06-03 18:58:55 +0200

    Update Potentiel.h

--- Potentiel.h
+++ Potentiel.h
@@ -4,21 +4,35 @@
 
 class Potentiel
 {
-	protected:
-	Vecteur2D poten; // Potentiel en lui-même 
 	
 	private:
 	Vecteur2D laplacien; // Laplacien qui lui est associé
-
+    Vecteur2D poten; // Potentiel en lui-même
 	
-	friend class ChampsPotentiels; // Permet à la classe Champs Potentiels d'accéder à ses attributs protégés
 	
 //--------------------------------------------------------------METHODES-------------------------------------------------------------//
 	
 	public:
-    Potentiel() = default;
+
+    // Constructeur initialisant les deux vecteurs à des vecteurs nuls
+    Potentiel();
+
+    // Constructeur d'un potentiel
     Potentiel(Vecteur2D const& p, Vecteur2D const& l) : poten(p), laplacien(l) {} // Constructeur d'un potentiel
 	
     void affiche(std :: ostream& sortie) const; // Affiche les attributs d'un potentiel
+
+    // Getteur de potentiel et laplacien
+    Vecteur2D& get_laplacien() const;
+    Vecteur2D& get_poten() const;
+
+    void set_laplacien(const double& x, const double& y);
+    void set_laplacien(const Vecteur2D& V);
+    void set_poten(const Vecteur2D& V);
+    void set_poten(const double& x, const double& y);
+    double laplacien_norme2() const;
 	
 };
+
+// Permet d'afficher un potentiel par surcharge de <<
+std::ostream& operator<<(std::ostream& sortie, Potentiel const& P);

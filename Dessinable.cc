a7efd8f671f7fdd822c97c75b0da037f41d0a19d
asselot <80883289+asselot@users.noreply.github.com>
2021-06-03 22:25:04 +0200

    Create Dessinable.cc

---
+++ Dessinable.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "SupportADessin.h"
+#include "Dessinable.h"
+using namespace std;
+
+class Dessinable; 
+
+Dessinable :: ~Dessinable()
+{
+
+cout << " on supprime un objet dessinable " << endl;
+
+}

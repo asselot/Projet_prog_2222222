86806d9356750e415b05bf35c4fcc1c6a5c042ed
clelialbrt <80390733+clelialbrt@users.noreply.github.com>
2021-06-03 20:05:06 +0200

    Update ChampsPotentiels.h

--- ChampsPotentiels.h
+++ ChampsPotentiels.h
@@ -5,49 +5,38 @@
 #include "Boite3D.h"
 #include "Potentiel.h"
 #include "Montagne.h"
+#include "Vecteur2D.h"
 
-class ChaineDeMontagnes ;
-
-class ChampsPotentiels : public Boite3D<Potentiel>{
+class ChampsPotentiels : public Boite3D<Potentiel>
+{
 
 	private:
-
-    // Retourne la somme des carrés des normes des vecteurs laplaciens
-    double erreur();
 	
-    void iteration(const ChaineDeMontagnes& chaine, const double& E = 0.1);
+	double erreur(); // Retourne la somme des carrés des normes des vecteurs laplaciens
+	
+	void iteration(const ChaineDeMontagnes& chaine, const double& E = 0.1); // Aucune idée
 
 	
 //--------------------------------------------------------------METHODES-------------------------------------------------------------//
 	
-	
 	public:
 	
-    // Constructeur à partir de coordonnées de la boîte (sans initialisation du tablO)
     ChampsPotentiels(unsigned int nox = 30, unsigned int noy = 30, unsigned int noz = 30, double p = 1.0);
-
-
-    // Constructeur à partir d'un autre champs (avec initialisation du tablO)
+    
     ChampsPotentiels(ChampsPotentiels const& C);
-
-    // destructeur du champs
+	
     ~ChampsPotentiels() {};
-
-    // Initialise les potentiels du champ potentiel
-    void initialise(double vi, const Montagne& mont);
+    
+	void initialise(double vi, const Montagne& mont); // Initialise les potentiels du champ potentiel
 	
-    // Retourne true si un potentiel est au bord de la boîte
-    //bool estauxbords(Potentiel pot) const;
+	//bool estauxbords(Potentiel pot) const; // Retourne true si un potentiel est au bord de la boîte
 	
-    // Calcule la valeur des laplaciens sur l'ensemble du champ à partir des potentiels
-    void calcule_laplaciens(const ChaineDeMontagnes& chaine);
-
-    void resolution (double const& seuil, unsigned int const&  max, bool const& verbeuse, const ChaineDeMontagnes& chaine);
+	void calcule_laplaciens(const ChaineDeMontagnes& chaine); // Calcule la valeur des laplaciens sur l'ensemble du champ à partir des potentiels 
 	
-    // Calcule la valeur de la vitesse du vent en un point donné
-    std::array<double, 3> vitesse(unsigned int const& i, unsigned int const& j, unsigned int const& k) const;
+	void resolution(double const& seuil, unsigned int const& max, bool const& verbeuse, const ChaineDeMontagnes& chaine); //bruh
 	
-    // Norme de la vitesse
-    double norme_vitesse(unsigned int const& pointi, unsigned int const&  pointj, unsigned int const&  pointk) const;
+	std::array<double, 3> vitesse(unsigned int const& i, unsigned int const& j, unsigned int const& k) const; // Calcule la valeur de la vitesse du vent en un point donné
+	
+	double norme_vitesse(unsigned int const& pointi, unsigned int const& pointj, unsigned int const& pointk) const; // Norme de la vitesse
 	
 };

caebec1bb6b607bf04417ae88c2717482296cfa7
asselot <80883289+asselot@users.noreply.github.com>
2021-06-03 22:22:37 +0200

    Update SupportADessin.h

--- SupportADessin.h
+++ SupportADessin.h
@@ -2,9 +2,10 @@
 #include <iostream>
 
 // Prototypes des classes nécessaires à la déclaration de SupportADessin afin d'éviter une inclusion circulaire
-class Montagne; 
 class Ciel;
 class Systeme;
+class Montagne;
+class ChaineDeMontagnes;
 
 
 // Super classe virtuelle qui représente la façon choisie pour dessiner (écran mode texte ou mode graphique)
@@ -20,10 +21,9 @@ class SupportADessin
 	// on suppose ici que les supports ne seront ni copiés ni déplacés
 	
 	// Méthode pure pour permettre aux différents objets d'être afficher
-	virtual void dessine(Systeme const&) = 0;
-	
-	virtual void dessine(Montagne const&) = 0;
-	
-	virtual void dessine(Ciel const&) = 0;
+    virtual void dessine(Systeme const& Sys) = 0;
+    virtual void dessine(Montagne const& montagne) = 0;
+    virtual void dessine(ChaineDeMontagnes const& chaine) = 0;
+    virtual void dessine(Ciel const& ciel) = 0;
 
 };

0b89b0dfaf22a60952e99fb8199f72fee8f28175
clelialbrt <80390733+clelialbrt@users.noreply.github.com>
2021-06-03 10:12:19 +0200

    Update Ciel.cc

--- Ciel.cc
+++ Ciel.cc
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include "Ciel.h"
 
@@ -9,6 +8,23 @@ using namespace std;
 // Demande à Sixtine
 void Ciel :: dessine_sur(SupportADessin& a_dessiner)
 { a_dessiner.dessine(*this); }
+
+/*void Ciel:: initialise(const ChampsPotentiels& champi)
+{
+	for (int i(0); i < champi.get_Nx(); ++i)
+		{
+			tablO.push_back(vector<vector<CubedAir>> ());
+			for (int j(0); j < champi.get_Ny(); ++j)
+			{
+				tablO[i].push_back(vector<CubedAir> ());
+				for (int k(0); k < champi.get_Nz(); ++k)
+				{
+					tablO[i][j].push_back(CubedAir ()); 
+					tablO[i][j][k].set_vitesse(champi.vitesse(i, j, k)[0], champi.vitesse(i, j, k)[1], champi.vitesse(i, j, k)[2]);
+				}
+			}
+		}
+}*/
 
 // Calcul du déplacement du nuage pendant un temps donné à partir d'un point donné
 CubedAir Ciel:: precedente(double pas_temps, double x, double y, double z)
@@ -24,7 +40,7 @@ CubedAir Ciel:: precedente(double pas_temps, double x, double y, double z)
 	// Si on se trouve au-delà de la boîte, l'état du nuage devient false (on ne se trouve plus dans le ciel)
 	if ((x > Nx) or (y > Ny) or (z > Nz))
 	{
-		tablO[x][y][z].etat == false;
+		tablO[x][y][z].etat(x, y, z) == false;
 		return tablO[x][y][z];
 	}
 	else
@@ -32,7 +48,3 @@ CubedAir Ciel:: precedente(double pas_temps, double x, double y, double z)
 		return tablO[x][y][z];
 	}
 }
-
-
-
-

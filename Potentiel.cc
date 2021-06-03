3064da61530c6a4445f40f10ce8d856daa46b17b
asselot <80883289+asselot@users.noreply.github.com>
2021-06-03 18:59:15 +0200

    Update Potentiel.cc

--- Potentiel.cc
+++ Potentiel.cc
@@ -12,4 +12,53 @@ void Potentiel:: affiche(ostream& sortie) const
     sortie << poten << " " << laplacien << endl;
 }
 
+// Constructeur d'un potentiel
+Potentiel :: Potentiel(Vecteur2D const& p, Vecteur2D const& l) : poten (p), laplacien (l) {}
 
+// Constructeur initialisant les deux vecteurs à des vecteurs nuls
+Potentiel :: Potentiel()
+    : poten(), laplacien()
+{}
+
+// Permet d'afficher un potentiel par surcharge de <<
+std::ostream& operator<<(std::ostream& sortie, Potentiel const& P)
+{
+    P.affiche(sortie);
+    return sortie;
+}
+
+Vecteur2D& Potentiel:: get_laplacien() const
+{
+    return laplacien;
+}
+Vecteur2D& Potentiel:: get_poten() const
+{
+    return poten;
+}
+
+double Vecteur2D :: laplacien_norme2() const
+{
+    return laplacien.norme2();
+}
+
+void Potentiel:: set_laplacien(const double& x, const double& y)
+{
+  laplacien.coord_x = x;
+  laplacien.coord_y = y;
+}
+
+void Potentiel:: set_poten(const Vecteur2D& V)
+{
+  poten = V;
+}
+
+void Potentiel:: set_poten(const double& x, const double& y)
+{
+  poten.coord_x = x;
+  poten.coord_y = y;
+}
+
+ void Potentiel:: set_laplacien(const Vecteur2D& V)
+ {
+     laplacien = V;
+ }

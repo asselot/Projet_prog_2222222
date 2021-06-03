8e80a8d960edb60fdb5706fb69cfd980e8d790d4
clelialbrt <80390733+clelialbrt@users.noreply.github.com>
2021-06-03 15:41:09 +0200

    Update CubedAir.h

--- CubedAir.h
+++ CubedAir.h
@@ -19,25 +19,27 @@ class CubedAir
 	
 	CubedAir(std::array<double, 3> vit_init) : vitesse_cubedair(vit_init) {} 
 	
-	double enthalpie(unsigned int pointx, unsigned int pointy, unsigned int pointz) const; // Calcule l'enthalpie en un point donné
+	double enthalpie(unsigned int pz, double pas) const; // Calcule l'enthalpie en un point donné
 	
-	double temperature(unsigned int px, unsigned int py, unsigned int pz) const; // Calcule la température en un point donné
+	double temperature(unsigned int pz, double pas) const; // Calcule la température en un point donné
 	
-	double pression(unsigned int px, unsigned int py, unsigned int pz) const; // Calcule la pression en un point donné
+	double pression(unsigned int pz, double pas) const; // Calcule la pression en un point donné
 	
-	double pression_partielle(unsigned int px, unsigned int py, unsigned int pz) const; // Calcule la pression de l'eau (Peau)
+	double pression_partielle(unsigned int pz, double pas) const; // Calcule la pression de l'eau (Peau)
 	
-	double pression_vapeur_saturante(unsigned int px, unsigned int py, unsigned int pz) const; // Calcule la pression de la rosée (Prosée)
+	double pression_vapeur_saturante(unsigned int pz, double pas) const; // Calcule la pression de la rosée (Prosée)
 	
-	bool etat(unsigned int px, unsigned int py, unsigned int pz) const; // Détermine l'état du cube d'air, elle retourne true si il est sous forme de nuage 
+	bool etat(unsigned int pz, double pas) const; // Détermine l'état du cube d'air, elle retourne true si il est sous forme de nuage 
 	
 	void set_vitesse(double coordx, double coordy, double coordz); // Associe les valeurs données aux coordonnées de la vitesse du cube 
 	
-    void affiche(std::ostream& sortie, unsigned int px, unsigned int py, unsigned int pz) const; // Affiche les différents attributs du cube d'air
+    void affiche(std::ostream& sortie, unsigned int pz, double pas) const; // Affiche les différents attributs du cube d'air
 	
 	double vitesse_norme() const; // Calcule la norme de la vitesse d'un cube d'air 
 	
-	void affiche_etat(unsigned int px, unsigned int py, unsigned int pz) const; // Affiche 1 si le cube est un nuage, 0 sinon
+	double v2() const;
+	
+	void affiche_etat(unsigned int pz, double pas) const; // Affiche 1 si le cube est un nuage, 0 sinon
 	
 	//ENTHALPIE ET CSTE DE BERNOUILLI CHANGENT 
 };

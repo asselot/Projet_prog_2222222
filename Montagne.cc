eb22e08a637e9a6f11edf338b574880f20b67c29
asselot <80883289+asselot@users.noreply.github.com>
2021-06-05 16:09:00 +0200

    Update Montagne.cc

--- Montagne.cc
+++ Montagne.cc
@@ -1,76 +1,147 @@
 #include <cmath>
 #include <iostream>
-#include <vector>
 #include "Montagne.h"
 #include "Dessinable.h"
 #include "SupportADessin.h"
+
+class ChaineDeMontagnes;
+
 using namespace std;
+//--------------------------------------------------------------CONSTRUCTEURS ET DESTRUCTEURS-------------------------------------------------------------//
 
-//--------------------------------------------------------------METHODES-------------------------------------------------------------//
+// Constructeur d'une montagne à partir d'un centre, une hauteur et un certain étalement
+Montagne :: Montagne(double const& x, double const& y, double const& h, double const& ox, double const& oy, int const& dim)
+    : x0(x), y0(y), H(h), ex(ox), ey(oy), dimension(dim) {}
 
-// Méthode virtuelle qui calcule l'altitude d'une montagne pure
-double Montagne:: altitude(double const& a, double const& b) const 
+// Constructeur d'une montagne à partir d'une montagne
+/*Montagne :: Montagne(const Montagne& montagne)
+    : Montagne(x0, y0, H, ex, ey, dimension)
+{}*/
+
+// Constructeur d'une chaine à partir d'une montagne
+ChaineDeMontagnes :: ChaineDeMontagnes(Montagne const& mont)
+    : Montagne(mont)
 {
-	double altitude(H*exp(-((a-x0)*(a-x0))/(2*ex*ex)-((b-y0)*(b-y0))/(2*ey*ey)));
-	if (altitude < 0.5) { return 0; }
-	else { return altitude; }
+    montagnes_simples.push_back(mont);
+    ++ taille;
+}
+// Constructeur d'une chaine à partir des élements d'une montagne
+ChaineDeMontagnes :: ChaineDeMontagnes(double const& x, double const& y, double const& h, double const& ox, double const& oy, int const& dim)
+{
+ Montagne M(x, y, h, ox, oy, dim);
+ montagnes_simples.push_back(M);
+ ++ taille ;
+
 }
 
-// Méthode virtuelle permettant de modifier les attributs d'une montagne avec les valeurs passées en arguments
-void Montagne:: set_montagne(double const& x, double const& y, double const& h, double const& ox, double const& oy) 
+// Constructeur d'une chaine à partir d'une autre chaine
+/*ChaineDeMontagnes :: ChaineDeMontagnes( ChaineDeMontagnes const& ch)
 {
-	x0 = x;
-	y0 = y;
-	H = h;
-	ex = ox;
-	ey = oy;
-}
+    montagnes_simples = ch.montagnes_simples;
+    taille = ch.get_taille();
+}*/
 
-std::ostream& operator<<(ostream& sortie, Montagne const& montagne)
-{
-	return montagne.affiche(sortie);
-}
+// Destructeur
+Montagne:: ~Montagne() {}
+
+// Destructeur
+ChaineDeMontagnes:: ~ChaineDeMontagnes() {}
+
+//--------------------------------------------------------------METHODES ET OPERATEURS POUR AFFICHER-------------------------------------------------------------//
 
 
-// Méthode dessine_sur redéfinie
-void Montagne::  dessine_sur(SupportADessin& a_dessiner)
-{
-	a_dessiner.dessine(*this);
-	
-}
-
+// Méthode affiche pour une montagne
 std::ostream& Montagne:: affiche(std::ostream& sortie) const
 {
-	sortie << " une montagne : " << endl;
-	sortie << " centre de la montagne (" << x0 << "," << y0 << ");" << endl;
-	sortie << " hauteur maximale : " << H << endl;
-	sortie << " étalement en x : " << ex << endl;
-	sortie << " étalement en y : " << ey << endl;
-	return sortie;
+    sortie << " une montagne : " << endl;
+    sortie << " centre de la montagne (" << x0 << "," << y0 << ");" << endl;
+    sortie << " hauteur maximale : " << H << endl;
+    sortie << " étalement en x : " << ex << endl;
+    sortie << " étalement en y : " << ey << endl;
+    return sortie;
 }
 
-// Méthode donnant aux attributs de la i-ème montagne simple de la chaîne les valeurs passées en arguments
-void ChaineDeMontagnes:: set_montagne(Montagne const& montagne) 
+// Surcharge de << pour une Montagne
+std::ostream& operator<<(ostream& sortie, Montagne const& montagne)
 {
-	montagnes_simples.push_back(montagne);
+    return montagne.affiche(sortie);
 }
 
-// Méthode virtuelle qui retourne le maximum des altitudes des montagnes simples et des sous-chaînes qui composent la chaîne 
-double ChaineDeMontagnes:: altitude(double const& a, double const& b) const 
+
+// Méthode affiche de la chaine
+void ChaineDeMontagnes:: affiche(ostream& sortie) const
 {
-	double retour(0);
+    for (auto const& element : montagnes_simples)
+    {
+        sortie << element;
+    }
 
-			for (size_t i(0); i < montagnes_simples.size(); ++i)
-			{
-				if (montagnes_simples[i].altitude(a, b) > retour)
-				{
-					retour = montagnes_simples[i].altitude(a, b); 
-				}
-			}
-
-		
-		
-	
-	return retour;
 }
 
+// Surcharge de << pour la chaine
+ostream& operator<<(ostream& sortie, ChaineDeMontagnes const& chaine)
+{
+    chaine.affiche(sortie);
+    return sortie;
+}
+
+//--------------------------------------------------------------METHODES MONTAGNE-------------------------------------------------------------//
+
+// Retourne la dimension de la boite - necessaire pour itérer sur les altitudes afin de dessiner la montagne - cf vue_opengl.cc
+int Montagne::  get_dimension() const
+{
+    return dimension;
+}
+
+// Méthode virtuelle qui calcule l'altitude d'une montagne
+double Montagne:: altitude(double const& a, double const& b) const
+{
+    double altitude(H*exp(-((a-x0)*(a-x0))/(2*ex*ex)-((b-y0)*(b-y0))/(2*ey*ey)));
+    if (altitude < 0.5) { return 0; }
+    else { return altitude; }
+}
+
+// Méthode dessine_sur() redéfinie héritée par Dessinable
+void Montagne:: dessine_sur(SupportADessin & support)
+{
+  support.dessine(*this);
+}
+//--------------------------------------------------------------METHODES CHAINE-------------------------------------------------------------//
+
+// Permet d'obtenir les montagnes simples de la chaine
+Montagne ChaineDeMontagnes:: get_montagnes_simples(int const& x) const
+{
+    return montagnes_simples[x];
+}
+
+// Retourne la taille de la montagne
+unsigned int ChaineDeMontagnes::  get_taille() const
+{
+    return taille;
+}
+
+// Méthode permettant d'ajouter des montagnes à la chaine
+void ChaineDeMontagnes:: set_montagne(Montagne const& montagne)
+{
+    montagnes_simples.push_back(montagne);
+    ++ taille;
+}
+
+// Méthode virtuelle qui retourne le maximum des altitudes des montagnes simples et des sous-chaînes qui composent la chaîne
+double ChaineDeMontagnes:: altitude(double const& a, double const& b) const
+{
+    double retour(0);
+
+            for (size_t i(0); i < montagnes_simples.size(); ++i)
+            {
+                if (montagnes_simples[i].altitude(a, b) > retour)
+                {
+                    retour = montagnes_simples[i].altitude(a, b);
+                }
+            }
+
+
+
+
+    return retour;
+}

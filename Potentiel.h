5d441514a826b690224368811f8f3bd02059681d
asselot <80883289+asselot@users.noreply.github.com>
2021-06-05 10:05:32 +0200

    Update Potentiel.h

--- Potentiel.h
+++ Potentiel.h
@@ -1,44 +1,50 @@
-#pragma once 
+#pragma once
 #include <iostream>
 #include "Vecteur2D.h"
 #include <array>
 
 class Potentiel
 {
-	
-	private:
-	Vecteur2D poten; // Potentiel en lui-même 
-	Vecteur2D laplacien; // Laplacien qui lui est associé
 
-	
+    private:
+    Vecteur2D poten; // Potentiel en lui-même
+    Vecteur2D laplacien; // Laplacien qui lui est associé
+
+
 //--------------------------------------------------------------METHODES-------------------------------------------------------------//
-	
-	public:
+
+    public:
+
     // Constructeur initialisant les deux vecteurs à des vecteurs nuls
     Potentiel();
-    
+
+    // Constructeur d'un potentiel à partir d'un autre potentiel
     Potentiel(Potentiel const& a_copier);
 
-    // Constructeur d'un potentiel
-    Potentiel(Vecteur2D const& p, Vecteur2D const& l);// Constructeur d'un potentiel
-	
-    std::ostream& affiche(std :: ostream& sortie) const; // Affiche les attributs d'un potentiel
-    
+    // Constructeur d'un potentiel à partir de vecteurs
+    Potentiel(Vecteur2D const& p, Vecteur2D const& l);
+
+    // Affiche les attributs d'un potentiel
+    std::ostream& affiche(std :: ostream& sortie) const;
+
+    // Retourne la vitesse des potentiels sous forme de tableau pour chacune des composantes de la vitesse
     std::array<double, 3> vitesse_potentiels(const Potentiel& imoins1jk, const Potentiel& ijmoins1k, const Potentiel& ijkmoins1, const Potentiel& iplus1jk, const Potentiel& ijplus1k, const Potentiel& ijkplus1) const;
-    
+
     void potentiel_iteration(const double& E);
-    
+
     void calcule_laplacien(const Potentiel& imoins1jk, const Potentiel& ijmoins1k, const Potentiel& ijkmoins1, const Potentiel& iplus1jk, const Potentiel& ijplus1k, const Potentiel& ijkplus1);
 
+    // Setteurs
     void set_laplacien(const double& x, const double& y);
     void set_laplacien(const Vecteur2D& V);
     void set_poten(const Vecteur2D& V);
     void set_poten(const double& x, const double& y);
+
+    // Retourne la norme au carré du laplacien
     double laplacien_norme2() const;
-	
+
 };
 
 // Permet d'afficher un potentiel par surcharge de <<
 std::ostream& operator<<(std::ostream& sortie, Potentiel const& P);
-	
 

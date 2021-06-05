0528daa6a68359de056f819629ac2a3cae7e8608
asselot <80883289+asselot@users.noreply.github.com>
2021-06-05 16:21:08 +0200

    Update Ciel.cc

--- Ciel.cc
+++ Ciel.cc
@@ -4,53 +4,7 @@
 
 using namespace std;
 
-//--------------------------------------------------------------METHODES-------------------------------------------------------------//
-
-// Méthode qui sert à dessiner le ciel héritée de Dessinable
-void Ciel :: dessine_sur(SupportADessin& a_dessiner)
-{ a_dessiner.dessine(*this); }
-
-// Calcul du déplacement du nuage pendant un temps donné à partir d'un point donné
-CubedAir Ciel:: precedente(double& pas_temps, double& x, double& y, double& z)
-{
-	//double norme_vitesse_cubedair(sqrt(tablO[x][y][z].vitesse_cubedair[0]*tablO[x][y][z].vitesse_cubedair[0] + tablO[x][y][z].vitesse_cubedair[1]*tablO[x][y][z].vitesse_cubedair[1] + tablO[x][y][z].vitesse_cubedair[2]*cubes_dair[x][y][z].vitesse_cubedair[2]));
-	
-	// Calcul du déplacement effectué et mise à jour des coordonnées
-	double deplacement(-pas_temps * tablO[x][y][z].vitesse_norme());
-	x += deplacement;
-	y += deplacement;
-	z += deplacement;
-	
-	// Si on se trouve au-delà de la boîte, l'état du nuage devient false (on ne se trouve plus dans le ciel)
-	if ((x > Nx) or (y > Ny) or (z > Nz))
-	{
-        tablO[x][y][z].etat(z, pas) == false;
-		return tablO[x][y][z];
-	}
-	else
-	{
-		return tablO[x][y][z];
-	}
-}
-
-
-// Initialise l'état des cubes d'air
-  void Ciel :: initialise()
-  {
-      for (unsigned int i(0); i < Nx; ++i)
-      {
-          for (unsigned int j(0); j < Ny; ++j)
-          {
-              for (unsigned int k(0); k < Nz; ++k)
-              {
-                  tablO[i][j][k].etat(k, pas);
-              }
-
-          }
-      }
-
-  }
-
+//--------------------------------------------------------------CONSTRUCTEURS-------------------------------------------------------------//
 
   // Constructeur du ciel à partir d'un champ de potentiels donné
   Ciel :: Ciel(ChampsPotentiels const& champ)
@@ -106,6 +60,52 @@ CubedAir Ciel:: precedente(double& pas_temps, double& x, double& y, double& z)
   tablO = C.get_tablO();
  }
 
+//--------------------------------------------------------------METHODES-------------------------------------------------------------//
+
+// Méthode qui sert à dessiner le ciel héritée de Dessinable
+void Ciel :: dessine_sur(SupportADessin& a_dessiner)
+{ a_dessiner.dessine(*this); }
+
+// Calcul du déplacement du nuage pendant un temps donné à partir d'un point donné
+CubedAir Ciel:: precedente(double& pas_temps, double& x, double& y, double& z)
+{
+	//double norme_vitesse_cubedair(sqrt(tablO[x][y][z].vitesse_cubedair[0]*tablO[x][y][z].vitesse_cubedair[0] + tablO[x][y][z].vitesse_cubedair[1]*tablO[x][y][z].vitesse_cubedair[1] + tablO[x][y][z].vitesse_cubedair[2]*cubes_dair[x][y][z].vitesse_cubedair[2]));
+	
+	// Calcul du déplacement effectué et mise à jour des coordonnées
+	double deplacement(-pas_temps * tablO[x][y][z].vitesse_norme());
+	x += deplacement;
+	y += deplacement;
+	z += deplacement;
+	
+	// Si on se trouve au-delà de la boîte, l'état du nuage devient false (on ne se trouve plus dans le ciel)
+	if ((x > Nx) or (y > Ny) or (z > Nz))
+	{
+        tablO[x][y][z].etat(z, pas) == false;
+		return tablO[x][y][z];
+	}
+	else
+	{
+		return tablO[x][y][z];
+	}
+}
+
+
+// Initialise l'état des cubes d'air
+  void Ciel :: initialise()
+  {
+      for (unsigned int i(0); i < Nx; ++i)
+      {
+          for (unsigned int j(0); j < Ny; ++j)
+          {
+              for (unsigned int k(0); k < Nz; ++k)
+              {
+                  tablO[i][j][k].etat(k, pas);
+              }
+
+          }
+      }
+
+  }
 
  // Affiche les paramètres du ciel
   void affiche(std::ostream& sortie) const
@@ -136,4 +136,7 @@ std::ostream& operator<<(std::ostream& sortie, const Ciel& ciel)
     ciel.affiche(sortie);
     return sortie;
 }
+
+
+
 

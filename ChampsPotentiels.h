69ec77e39d9808025f41eba63529bfaa43ec6519
asselot <80883289+asselot@users.noreply.github.com>
2021-06-05 09:44:22 +0200

    Update ChampsPotentiels.h

--- ChampsPotentiels.h
+++ ChampsPotentiels.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <array>
 #include "constantes.h"
 #include "Boite3D.h"
@@ -10,37 +9,44 @@
 class ChampsPotentiels : public Boite3D<Potentiel>
 {
 
-	private:
-	
-	double erreur(); // Retourne la somme des carrés des normes des vecteurs laplaciens
-	
-	void iteration(const ChaineDeMontagnes& chaine, const double& E = 0.1); // Aucune idée
+    private:
 
-	
+    double erreur(); // Retourne la somme des carrés des normes des vecteurs laplaciens
+
+    void iteration(const ChaineDeMontagnes& chaine, const double& E = 0.1); // Aucune idée
+
 //--------------------------------------------------------------METHODES-------------------------------------------------------------//
-	
-	public:
-	
+
+    public:
+
+    // Constructeur à partir des dimensions de la boîte
     ChampsPotentiels(unsigned int nox = 30, unsigned int noy = 30, unsigned int noz = 30, double p = 1.0);
-    
+
+    // Constructeur à partir d'es dimensions de la boîte'un autre champ
     ChampsPotentiels(ChampsPotentiels const& C);
-	
-    ~ChampsPotentiels() {};
-    
-	void initialise(double vi, const Montagne& mont); // Initialise les potentiels du champ potentiel
-	
-	//bool estauxbords(Potentiel pot) const; // Retourne true si un potentiel est au bord de la boîte
-	
-	void calcule_laplaciens(const ChaineDeMontagnes& chaine); // Calcule la valeur des laplaciens sur l'ensemble du champ à partir des potentiels 
-	
-	void resolution(double const& seuil, unsigned int const& max, bool const& verbeuse, const ChaineDeMontagnes& chaine); //bruh
-	
-	std::array<double, 3> vitesse(unsigned int const& i, unsigned int const& j, unsigned int const& k) const; // Calcule la valeur de la vitesse du vent en un point donné
-	
-	double norme_vitesse(unsigned int const& pointi, unsigned int const& pointj, unsigned int const& pointk) const; // Norme de la vitesse
-	
-	void affiche(std::ostream& sortie) const;
-	
+
+    // Destructeur de la classe
+    ~ChampsPotentiels();
+
+    // Initialise les potentiels du champ potentiel
+    void initialise(double vi, const Montagne& mont);
+
+    // Calcule la valeur des laplaciens sur l'ensemble du champ à partir des potentiels
+    void calcule_laplaciens(const ChaineDeMontagnes& chaine);
+
+    void resolution(double const& seuil, unsigned int const& max, bool const& verbeuse, const ChaineDeMontagnes& chaine);
+
+    // Retourne la vitesse du vent en un point donné sous forme de tableau dont les composants représentent les composantes de la vitesse en x, y, z
+    std::array<double, 3> vitesse(unsigned int const& i, unsigned int const& j, unsigned int const& k) const;
+
+    // Retourne la norme de la vitesse
+    double norme_vitesse(unsigned int const& pointi, unsigned int const& pointj, unsigned int const& pointk) const;
+
+    // Permet d'afficher les paramètres du champ
+    void affiche(std::ostream& sortie) const;
+
 };
 
+// Surcharge de l'opérateur <<
 std::ostream& operator<<(std::ostream& sortie, const ChampsPotentiels& champ);
+
